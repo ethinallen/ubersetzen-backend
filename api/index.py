74403e18d0cb500c1fd7e42896cdb3839c7390db
Drew <abemery@clemson.edu>
2021-11-02 12:13:17 -0400

    remove vercel.json; add requests to requirements

--- api/index.py
+++ api/index.py
@@ -1,32 +1,13 @@
 from flask import Flask
 from http.server import BaseHTTPRequestHandler
 from urllib import parse
-
+import requests
 from flask import Flask
 
 
+
+
 app = Flask(__name__)
-
-
-@app.route('/')
-def home():
-    return '<h1>Home Page Route</h1>'
-
-
-@app.route('/about')
-def about():
-    return 'About Page Route'
-
-
-@app.route('/portfolio')
-def portfolio():
-    return 'Portfolio Page Route'
-
-
-@app.route('/contact')
-def contact():
-    return 'Contact Page Route'
-
 
 @app.route('/api')
 def api():
