fe996fa23451ec5c16082db3e12ec446d20a45c4
Drew <abemery@clemson.edu>
2021-11-02 11:57:22 -0400

    remove template code for random code on towards data science

--- api/index.py
+++ api/index.py
@@ -3,22 +3,22 @@ from http.server import BaseHTTPRequestHandler
 from urllib import parse
 import requests
 
-class handler(BaseHTTPRequestHandler):
-	def do_GET(self):
-
-		s = self.path
-		dic = dict(parse.parse_qsl(parse.urlsplit(s).query))
-		self.send_response(200)
-		self.send_header('Content-type','text/plain')
-		self.end_headers()
-		print(dic)
-		if "name" in dic:
-			message = "Hello, " + dic["name"] + "!"
-		else:
-			message = "Hello, stranger!"
-
-		self.wfile.write(message.encode())
-		return
+# class handler(BaseHTTPRequestHandler):
+# 	def do_GET(self):
+#
+# 		s = self.path
+# 		dic = dict(parse.parse_qsl(parse.urlsplit(s).query))
+# 		self.send_response(200)
+# 		self.send_header('Content-type','text/plain')
+# 		self.end_headers()
+# 		print(dic)
+# 		if "name" in dic:
+# 			message = "Hello, " + dic["name"] + "!"
+# 		else:
+# 			message = "Hello, stranger!"
+#
+# 		self.wfile.write(message.encode())
+# 		return
 from flask import Flask
 
 
