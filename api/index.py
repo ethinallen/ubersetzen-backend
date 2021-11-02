e0be00e805f6428658bc1ab6518dd8268388ddc6
Drew <abemery@clemson.edu>
2021-11-02 09:43:31 -0400

    obvious reasons that broke..

--- api/index.py
+++ api/index.py
@@ -4,17 +4,16 @@ import requests
 
 class handler(BaseHTTPRequestHandler):
 	def do_GET(self):
-		message = requests.get('www.google.com')
 
 		s = self.path
 		dic = dict(parse.parse_qsl(parse.urlsplit(s).query))
 		self.send_response(200)
 		self.send_header('Content-type','text/plain')
 		self.end_headers()
-		# if "name" in dic:
-		# 	message = "Hello, " + dic["name"] + "!"
-		# else:
-		# 	message = "Hello, stranger!"
+		if "name" in dic:
+			message = "Hello, " + dic["name"] + "!"
+		else:
+			message = "Hello, stranger!"
 
 		self.wfile.write(message.encode())
 		return
