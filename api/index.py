36ec8206933c5f25619d9910f28db518f98ac83d
Drew <abemery@clemson.edu>
2021-11-02 11:49:43 -0400

    trying flask again?

--- api/index.py
+++ api/index.py
@@ -3,25 +3,50 @@ from http.server import BaseHTTPRequestHandler
 from urllib import parse
 import requests
 
+class handler(BaseHTTPRequestHandler):
+	def do_GET(self):
+
+		s = self.path
+		dic = dict(parse.parse_qsl(parse.urlsplit(s).query))
+		self.send_response(200)
+		self.send_header('Content-type','text/plain')
+		self.end_headers()
+		print(dic)
+		if "name" in dic:
+			message = "Hello, " + dic["name"] + "!"
+		else:
+			message = "Hello, stranger!"
+
+		self.wfile.write(message.encode())
+		return
+from flask import Flask
+
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def home():
     return 'Home Page Route'
 
-# class handler(BaseHTTPRequestHandler):
-# 	def do_GET(self):
-#
-# 		s = self.path
-# 		dic = dict(parse.parse_qsl(parse.urlsplit(s).query))
-# 		self.send_response(200)
-# 		self.send_header('Content-type','text/plain')
-# 		self.end_headers()
-# 		print(dic)
-# 		if "name" in dic:
-# 			message = "Hello, " + dic["name"] + "!"
-# 		else:
-# 			message = "Hello, stranger!"
-#
-# 		self.wfile.write(message.encode())
-# 		return
+
+@app.route('/about')
+def about():
+    return 'About Page Route'
+
+
+@app.route('/portfolio')
+def portfolio():
+    return 'Portfolio Page Route'
+
+
+@app.route('/contact')
+def contact():
+    return 'Contact Page Route'
+
+
+@app.route('/api')
+def api():
+    with open('data.json', mode='r') as my_file:
+        text = my_file.read()
+        return text
