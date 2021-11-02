44cfba3884827eefc2cba7b6dee4858e6f5ad04d
Drew <abemery@clemson.edu>
2021-11-02 10:17:18 -0400

    trying flask

--- api/index.py
+++ api/index.py
@@ -1,19 +1,27 @@
+from flask import Flask
 from http.server import BaseHTTPRequestHandler
 from urllib import parse
 import requests
 
-class handler(BaseHTTPRequestHandler):
-	def do_GET(self):
+app = Flask(__name__)
 
-		s = self.path
-		dic = dict(parse.parse_qsl(parse.urlsplit(s).query))
-		self.send_response(200)
-		self.send_header('Content-type','text/plain')
-		self.end_headers()
-		if "name" in dic:
-			message = "Hello, " + dic["name"] + "!"
-		else:
-			message = "Hello, stranger!"
+@app.route('/')
+def home():
+    return 'Home Page Route'
 
-		self.wfile.write(message.encode())
-		return
+# class handler(BaseHTTPRequestHandler):
+# 	def do_GET(self):
+#
+# 		s = self.path
+# 		dic = dict(parse.parse_qsl(parse.urlsplit(s).query))
+# 		self.send_response(200)
+# 		self.send_header('Content-type','text/plain')
+# 		self.end_headers()
+# 		print(dic)
+# 		if "name" in dic:
+# 			message = "Hello, " + dic["name"] + "!"
+# 		else:
+# 			message = "Hello, stranger!"
+#
+# 		self.wfile.write(message.encode())
+# 		return
