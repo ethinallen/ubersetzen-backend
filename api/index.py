b586aa7ef2550c2913fe4d52cb8f9c5725660b63
Drew <abemery@clemson.edu>
2021-11-02 12:00:59 -0400

    is the article bad or am i bad? could it be both?

--- api/index.py
+++ api/index.py
@@ -3,22 +3,6 @@ from http.server import BaseHTTPRequestHandler
 from urllib import parse
 import requests
 
-# class handler(BaseHTTPRequestHandler):
-# 	def do_GET(self):
-#
-# 		s = self.path
-# 		dic = dict(parse.parse_qsl(parse.urlsplit(s).query))
-# 		self.send_response(200)
-# 		self.send_header('Content-type','text/plain')
-# 		self.end_headers()
-# 		print(dic)
-# 		if "name" in dic:
-# 			message = "Hello, " + dic["name"] + "!"
-# 		else:
-# 			message = "Hello, stranger!"
-#
-# 		self.wfile.write(message.encode())
-# 		return
 from flask import Flask
 
 
@@ -27,7 +11,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Home Page Route'
+    return '<h1>Home Page Route</h1>'
 
 
 @app.route('/about')
