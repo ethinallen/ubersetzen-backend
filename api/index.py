8a1872f1fcf90d64415b6c4f9e3f3fadfa7e0550
Drew <abemery@clemson.edu>
2021-11-02 12:22:29 -0400

    return token to api route

--- api/index.py
+++ api/index.py
@@ -3,12 +3,25 @@ from http.server import BaseHTTPRequestHandler
 from urllib import parse
 import requests
 from flask import Flask
+import lyricsGenius
+import os
 
+def getToken():
+	data = {
+	  'client_id': 'TjidTt_p0RhWw65nuJpEJH8OoqHjylagsuuOOebC8DTRjw1dgPQ1Gi2phyV1kS28',
+	  'client_secret': os.environ['GENIUS_SECRET'],
+	  'grant_type': 'client_credentials'
+	}
 
+	response = requests.post('https://api.genius.com/oauth/token', data=data)
+	print(response['access_token'])
+	token = response
 
+# def getGeniusLyrics(title, artist):
+# 	response =
 
 app = Flask(__name__)
 
 @app.route('/api')
 def api():
-    return "dingle dangle"
+    return getToken()
